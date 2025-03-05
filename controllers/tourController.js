e932431b87c13813f35b7fbc1843202634a96cd4
tonyishangu <tonyishangu@gmail.com>
2025-03-05 20:39:18 +0300

    rest apis for getting all tours and getting a tour by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,33 +2,45 @@ const Tour = require("../models/tourModel")
 
 
 // tours route handlers
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'Success',
-    requestedAt: req.requsetTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try{
+    const tours = await Tour.find()
+    res.status(200).json({
+      status: 'Success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch(err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err
+    })
+  }
 };
 
-exports.getTourById = (req, res) => {
-  const id = req.params.id * 1;
-  // const tour = tours.find((el) => el.id === id);
+exports.getTourById = async (req, res) => {
+ try {
+  const tour = await Tour.findById(req.params.id)
+  // Tour.findOne({ _id: req.params.id})
+   res.status(201).json({
+     status: 'success',
+     data: {
+       tour,
+     },
+   });
+ } catch (error) {
+  res.status(400).json({
+    status: 'fail',
+    message: 'Invalid ID'
+  })
+ }
 
-  // res.status(201).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
 };
 
 exports.createTour = async (req, res) => {
   try{
-    // const newTour = new Tour({})
-    // newTour.save()
     const newTour = await Tour.create(req.body)
   
         res.status(201).json({
