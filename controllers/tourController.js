678b5fdedd1415432666b451c8d34ce5c3c8a772
tonyishangu <tonyishangu@gmail.com>
2025-03-10 22:45:36 +0300

    sorting feature implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,20 +4,29 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
-    // 1.Filtering
+    // 1a. Filtering
     const queryObj = { ...req.query }
     const excludedFields = ['page', 'sort', 'limit', 'fields']
     excludedFields.forEach(el => delete queryObj[el])
 
-    // Advance Filtering
+    // 1b. Advance Filtering
     // {Difficulty: 'easy', duration: {$gte: 5}}
     let queryStr = JSON.stringify(queryObj)
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
     console.log(JSON.parse(queryStr));
     
-    const query =  Tour.find(JSON.parse(queryStr));
-
+    let query =  Tour.find(JSON.parse(queryStr));
     // const query = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
+
+    // 2. Sorting
+    if(req.query.sort){
+      const sortBy = req.query.sort.split(',').join(' ')
+      query = query.sort(sortBy)
+      // sort('price ratingsAverage')
+    } else{
+      query = query.sort('-createdAt')
+    }
+
     // EXECUTE QUERY
     const tours = await query
     // SEND RESPONCE
