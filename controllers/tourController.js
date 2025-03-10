5fa917c0957dc5f1421fb58ac552b997cf9de0eb
tonyishangu <tonyishangu@gmail.com>
2025-03-10 22:05:16 +0300

    basic filtering using query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,6 +14,7 @@ exports.getAllTours = async (req, res) => {
     
     // EXECUTE QUERY
     const tours = await query
+    // SEND RESPONCE
     res.status(200).json({
       status: 'Success',
       results: tours.length,
