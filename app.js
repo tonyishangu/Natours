5de86b0657f6666d4a2822a58f094ae03ee6b591
tonyishangu <tonyishangu@gmail.com>
2025-03-20 19:48:55 +0300

    route error handling middleware implemented

--- app.js
+++ app.js
@@ -23,4 +23,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', usersRouter)
 
+app.all('*',(req,res,next) => {
+  res.status(404).json({
+    status: 'Fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  })
+})
+
 module.exports = app
