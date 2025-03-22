c97a0b4e4ccde545d844885dc7c80d6f715fbc20
tonyishangu <tonyishangu@gmail.com>
2025-03-22 19:11:15 +0300

    global error handling middleware

--- app.js
+++ app.js
@@ -24,9 +24,24 @@ app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', usersRouter)
 
 app.all('*',(req,res,next) => {
-  res.status(404).json({
-    status: 'Fail',
-    message: `Can't find ${req.originalUrl} on this server`
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // })
+  const err = new Error (`Can't find ${req.originalUrl} on this server`)
+  err.status = 'Fail'
+  err.statusCode = 400
+
+  next(err)
+
+})
+
+app.use((err, req, res, next) =>{
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   })
 })
 
