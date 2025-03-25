1ea4341e432a7125e29a3188a39c8768f8c25e27
tonyishangu <tonyishangu@gmail.com>
2025-03-25 23:30:46 +0300

    handling mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,20 @@
-const AppError = require("../utils/appError")
+const AppError = require('../utils/appError');
 
-const handleCastErrorDB = err => {
-  const message = `Invalid ${err.path}: ${err.value}`
-  return new AppError(message, 400)
-}
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
 
-const handleDuplicateFieldsDB = err => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
-  console.log('value',value)
-  const message = `Duplicate field value: ${value} please use another value`
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log('value', value);
+  const message = `Duplicate field value: ${value} please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message)
+  const message = `Invalid input data. ${errors.join('. ')}`
   return new AppError(message, 400)
 }
 
@@ -17,41 +23,40 @@ const sendErrorDev = (err, res) => {
     status: err.status,
     message: err.message,
     stack: err.stack,
-    error: err
-  })
-}
+    error: err,
+  });
+};
 
 const sendErrorProd = (err, res) => {
-  if(err.isOperational){
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message
-    })
+      message: err.message,
+    });
   } else {
     // 1, log the error
-    console.error('Error', err)
+    console.error('Error', err);
     // 2, send a generic message
     res.status(500).json({
       status: 'error',
-      message: 'Something went wrong'
-    })
+      message: 'Something went wrong',
+    });
   }
-}
+};
 
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack)
 
-module.exports = (err, req, res, next) =>{
-    // console.log(err.stack)
-  
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || 'error'
-    
-    if(process.env.NODE_ENV === 'development'){
-     sendErrorDev(err, res)
-    } else if(process.env.NODE_ENV === 'production') {
-      let error = {...err}
-      if(error.name === 'CastError') error = handleCastErrorDB(error)
-        if(error.code === 11000) error = handleDuplicateFieldsDB(error)
-     sendErrorProd(error, res)
-    }
-}  
-   
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if(error.name === 'ValidationError') error = handleValidationErrorDB(error)
+    sendErrorProd(error, res);
+  }
+};
