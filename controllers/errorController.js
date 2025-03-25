a5fa8618c03e2e7bd49c9eb6f87ef2c3a87d6ef1
tonyishangu <tonyishangu@gmail.com>
2025-03-25 22:32:51 +0300

    setting up error messages for development and production stages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err
+  })
+}
+
+const sendErrorProd = (err, res) => {
+  if(err.isOperational){
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    })
+  } else {
+    // 1, log the error
+    console.error('Error', err)
+    // 2, send a generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    })
+  }
+}
+
+
 module.exports = (err, req, res, next) =>{
     // console.log(err.stack)
   
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
     
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    })
-  }
+    if(process.env.NODE_ENV === 'development'){
+     sendErrorDev(err, res)
+    } else if(process.env.NODE_ENV === 'production') {
+     sendErrorProd(err, res)
+    }
+}  
+   
