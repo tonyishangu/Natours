357f0e1b5da92476a2d25117603c93596bef6a0e
tonyishangu <tonyishangu@gmail.com>
2025-04-02 11:46:06 +0300

    created a model and schema for adding new users

---
+++ models/usermodel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true,'Name is rquired']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please give a valid email']
+    },
+    photo: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: [true, 'Enter a password'],
+        minlength: 6
+    },
+    passwordConfirm:{
+        type: String,
+        required: [true, 'Re-enter the password']
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
