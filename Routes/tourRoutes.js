82ee33f1f4fa712042f8d7f92b917d48d9687a7c
tonyishangu <tonyishangu@gmail.com>
2025-04-23 21:07:57 +0300

    implement authorization

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const tourController = require('../controllers/tourController');
-const authController = require('../controllers/authController')
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
@@ -10,8 +10,8 @@ const router = express.Router();
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
-router.route('/tour-stats').get(tourController.getTourStats)
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/')
   .get(authController.protect, tourController.getAllTours)
@@ -20,6 +20,10 @@ router
   .route('/:id')
   .get(tourController.getTourById)
   .patch(tourController.editTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'leadGuide'),
+    tourController.deleteTour,
+  );
 
 module.exports = router;
--- controllers/authController.js
+++ controllers/authController.js
@@ -5,8 +5,8 @@ const User = require('../models/usermodel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-const signToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+const signToken = id => jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
   });
 
 exports.signup = catchAsync(async (req, res, next) => {
@@ -48,7 +48,7 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
-  // getting token and checkif it's there
+  // 1) Getting token and check of it's there
   let token;
   if (
     req.headers.authorization &&
@@ -56,25 +56,46 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  // console.log(token)
+
   if (!token) {
     return next(
-      new AppError('User not logged in, please login to view tours', 401),
+      new AppError('You are not logged in! Please log in to get access.', 401)
     );
   }
-  // verfication token
+
+  // 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  // check if user still exists
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
-    return next(new AppError('The user no longer exists', 401));
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401
+      )
+    );
   }
-  // check if user changed password after the token was issued
-  if(freshUser.changesPasswordAfter(decoded.iat)){
-    return next(new AppError('Password Changed, please log in again', 401))
+
+  // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
   }
 
 //   grant access to protected routes
-req.user = freshUser
+req.user = currentUser
   next();
 });
+
+exports.restrictTo = (...roles) => (req, res, next) => {
+    // roles ['admin', 'lead-guide']. role='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
